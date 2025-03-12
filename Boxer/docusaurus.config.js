af61b45b21b6b54f002e80924191e8c062781c0d
Kaiyuan Qian <116858989+kaiyuanqian@users.noreply.github.com>
2025-03-12 14:51:37 +1100

    Make personalisation changes as test

--- Boxer/docusaurus.config.js
+++ Boxer/docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Boxer Documentation',
+  tagline: 'Warehouse inventory management solution',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
