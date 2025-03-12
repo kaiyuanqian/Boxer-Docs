294df74011a4778f7224d46e4bd3e1df0d09f840
Kaiyuan Qian <116858989+kaiyuanqian@users.noreply.github.com>
2025-03-12 14:34:09 +1100

    changes

--- Boxer/docusaurus.config.js
+++ Boxer/docusaurus.config.js
@@ -15,15 +15,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://boxer.github.io',
+  url: 'https://boxer-docs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Boxer-Docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'kaiyuanqian', // Usually your GitHub org/user name.
-  projectName: 'boxer', // Usually your repo name.
+  projectName: 'Boxer-Docs', // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
